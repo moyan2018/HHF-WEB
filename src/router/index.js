489ba59e949a4d2d773f016c8b470ea6c6cc0575
hanhaifei <moyan@orbbec.com>
2019-06-20 18:31:29 +0800

    first

---
+++ src/router/index.js
@@ -0,0 +1,135 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import LeftNav from '@/components/nav/leftNav.vue'
+import Home from '@/views/home.vue'
+import EnterpriseList from '@/views/enterprise/index.vue'
+import EnterpriseAdd from '@/views/enterprise/add.vue'
+import EnterpriseDetail from '@/views/enterprise/detail.vue'
+import VehicleManage from '@/views/vehicle/index.vue'
+import DeptManager from '@/views/dept/index.vue'
+import NotFound from '@/components/404.vue'
+
+// 懒加载方式，当路由被访问的时候才加载对应组件
+const Login = resolve => require(['@/views/login'], resolve)
+
+Vue.use(Router)
+
+let router = new Router({
+  routes: [
+    {
+      path: '/login',
+      type: 'login',
+      component: Login
+    },
+    {
+      path: '*',
+      component: NotFound
+    },
+    {
+      path: '/',
+      type: 'enterprise',
+      name: 'enterprise',
+      component: Home,
+      redirect: '/enterprise/list',
+      menuShow: true,
+      children: [
+        {
+          path: '/enterpriseList',
+          component: LeftNav,
+          name: '企业管理',
+          leaf: true, // 只有一个节点
+          iconCls: 'iconfont icon-home', // 图标样式class
+          menuShow: true,
+          children: [
+            { path: '/enterprise/list', component: EnterpriseList, name: '企业列表', menuShow: true }
+          ]
+        },
+        {
+          path: '/enterpriseAdd',
+          component: LeftNav,
+          name: 'enterpriseAdd',
+          leaf: true, // 只有一个节点
+          iconCls: 'el-icon-menu',
+          menuShow: true,
+          children: [
+            { path: '/enterprise/add', component: EnterpriseAdd, name: '企业添加', menuShow: true }
+          ]
+        },
+        {
+          path: '/enterpriseDetail',
+          component: LeftNav,
+          name: 'enterpriseDetail',
+          leaf: true, // 只有一个节点
+          iconCls: 'el-icon-menu',
+          menuShow: true,
+          children: [
+            { path: '/enterprise/detail', component: EnterpriseDetail, name: '企业详情', menuShow: true }
+          ]
+        },
+      ]
+    },
+    {
+      path: '/vehicleManager',
+      type: 'enterprise',
+      name: 'vehicle',
+      component: Home,
+      redirect: '/vehicle/list',
+      menuShow: true,
+      children: [
+        {
+          path: '/vehicleList',
+          component: LeftNav,
+          name: 'vehicleList',
+          leaf: true, // 只有一个节点
+          iconCls: 'iconfont icon-home', // 图标样式class
+          menuShow: true,
+          children: [
+            { path: '/vehicle/list', component: VehicleManage, name: '车辆信息', menuShow: true }
+          ]
+        }
+      ]
+    },
+    {
+      path: '/deptManager',
+      type: 'enterprise',
+      name: 'dept',
+      component: Home,
+      redirect: '/dept/list',
+      menuShow: true,
+      children: [
+        {
+          path: '/deptList',
+          component: LeftNav,
+          name: 'deptList',
+          leaf: true, // 只有一个节点
+          iconCls: 'iconfont icon-home', // 图标样式class
+          menuShow: true,
+          children: [
+            { path: '/dept/list', component: DeptManager, name: '部门信息', menuShow: true }
+          ]
+        }
+      ]
+    }
+
+  ]
+});
+
+router.beforeEach((to, from, next) => {
+  // console.log('to:' + to.path)
+  if (to.path.startsWith('/login')) {
+    window.localStorage.removeItem('access-user')
+    next()
+  } else if(to.path.startsWith('/register')){
+    window.localStorage.removeItem('access-user')
+    next()
+  } else {
+    let user = JSON.parse(window.localStorage.getItem('access-user'))
+    if (!user) {
+      next({path: '/login'})
+    } else {
+      next()
+    }
+  }
+});
+
+export default router
---
+++ src/views/home.vue
@@ -0,0 +1,126 @@
+<template>
+  <el-row class="container">
+    <!--头部-->
+    <el-col :span="24" class="topbar-wrap">
+      <div class="topbar-logos">
+        <span v-show="!$store.state.collapsed" style="color: #fff;">后台管理系统</span>
+      </div>
+      <div class="topbar-title">
+        <el-row v-show="$store.state.topNavState==='enterprise'">
+          <el-col :span="24">
+            <el-menu :default-active="defaultActiveIndex" class="el-menu-demo" mode="horizontal" @select="handleSelect" :router="true">
+              <el-menu-item index="/">企业信息</el-menu-item>
+              <el-menu-item index="/vehicleManager">车辆信息</el-menu-item>
+              <el-menu-item index="/deptManager">组织架构</el-menu-item>
+            </el-menu>
+          </el-col>
+        </el-row>
+      </div>
+      <div class="topbar-account topbar-btn">
+        <el-dropdown trigger="click">
+            <img src="../assets/logo.gif" style="border-radius: 7px;width: 45px;margin: 2px">
+          <el-dropdown-menu slot="dropdown">
+            <el-dropdown-item>
+              <div @click="jumpTo('/user/profile')"><span style="color: #555;font-size: 14px;">个人信息</span></div>
+            </el-dropdown-item>
+            <el-dropdown-item>
+              <div @click="jumpTo('/user/changepwd')"><span style="color: #555;font-size: 14px;">修改密码</span></div>
+            </el-dropdown-item>
+            <el-dropdown-item divided @click.native="logout">退出登录</el-dropdown-item>
+          </el-dropdown-menu>
+        </el-dropdown>
+      </div>
+    </el-col>
+
+    <!--中间-->
+    <transition name="fade" mode="out-in">
+      <router-view></router-view>
+    </transition>
+  </el-row>
+</template>
+<script>
+  export default {
+    name: 'home',
+    data () {
+      return {
+        defaultActiveIndex: "/",
+        loading: false,
+        nickname: ''
+      }
+    },
+    created() {// 组件创建完后获取数据，
+      // 此时 data 已经被 observed 了
+      this.fetchNavData();
+    },
+    methods: {
+      handleSelect(index){
+        this.defaultActiveIndex = index;
+      },
+      fetchNavData () { // 初始化菜单激活项
+        var cur_path = this.$route.path; //获取当前路由
+        var routers = this.$router.options.routes; // 获取路由对象
+        var nav_type = "", nav_name = "";
+        for (var i = 0; i < routers.length; i++) {
+          var children = routers[i].children;
+          if(children){
+            for (var j = 0; j < children.length; j++) {
+              var grand_children = children[j].children;
+              if(grand_children){
+                for (var k = 0; k < grand_children.length; k++) {
+                  if (grand_children[k].path === cur_path) {
+                    nav_type = routers[i].type;
+                    nav_name = routers[i].name;
+                    break;
+                  }
+                  // 如果该菜单下还有子菜单
+                  if(children[j].children) {
+                    let grandChildren = children[j].children;
+                    for(let z=0; z<grandChildren.length; z++) {
+                      if(grandChildren[z].path === cur_path) {
+                        nav_type = routers[i].type;
+                        nav_name = routers[i].name;
+                        break;
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+        this.$store.state.topNavState = nav_type;  // 改变topNavState状态的值
+        this.$store.state.leftNavState = nav_name;  // 改变leftNavState状态的值
+        if(nav_type == "home"){
+          this.defaultActiveIndex = "/";
+        } else {
+          this.defaultActiveIndex = "/" + nav_name + "Manager";
+        }
+      },
+      jumpTo(url){
+        this.defaultActiveIndex = url;
+        this.$router.push(url); //用go刷新
+      },
+      logout(){
+        //logout
+        let that = this;
+        this.$confirm('确认退出吗?', '提示', {
+          confirmButtonClass: 'el-button--warning'
+        }).then(() => {
+          //确认
+          localStorage.removeItem('access-user');
+          that.$router.go('/login'); //用go刷新
+        }).catch(() => {});
+      }
+    },
+    mounted() {
+      let user = localStorage.getItem('access-user');
+      if (user) {
+        user = JSON.parse(user);
+        this.nickname = user.nickname || '';
+      }
+    },
+    watch: {
+      '$route': 'fetchNavData'  //监听router值改变时，改变导航菜单激活项
+    }
+  }
+</script>
